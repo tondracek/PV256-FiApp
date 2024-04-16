bbb19a7d5edce0280cab27b877676bd7a5162fed
stefankrajanec <stevo.krajanec@gmail.com>
2024-04-16 09:08:49 +0200

    network permission

--- app/src/main/java/com/example/fiapp/lala/UserClient.kt
+++ app/src/main/java/com/example/fiapp/lala/UserClient.kt
@@ -7,7 +7,7 @@ import parad0x.fi.proto.LoginServiceGrpcKt
 class UserClient() {
     private val port = 50081
 
-    private val channel = ManagedChannelBuilder.forAddress("https://89.221.215.177", port).usePlaintext().build()
+    private val channel = ManagedChannelBuilder.forAddress("89.221.215.177", port).usePlaintext().build()
     private val stub = LoginServiceGrpcKt.LoginServiceCoroutineStub(channel)
 
     // z hladiska clean arch nie je spravne použivať builder objekty ako parameter do klienta v oboch pripadoch vytvorte nový objekt a tu ho premapujte na bulder objekt a zbuildite ho
