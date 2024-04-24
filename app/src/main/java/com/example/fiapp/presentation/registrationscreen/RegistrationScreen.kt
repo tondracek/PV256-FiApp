b9c789871d94ba2ec0825b7a8aa8b61d87471a37
Tomáš Ondráček <t.ondracek187@gmail.com>
2024-04-24 10:18:38 +0200

    code clean up

--- app/src/main/java/com/example/fiapp/presentation/registrationscreen/RegistrationScreen.kt
+++ app/src/main/java/com/example/fiapp/presentation/registrationscreen/RegistrationScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
